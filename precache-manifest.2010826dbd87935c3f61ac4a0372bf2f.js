8ead82e4e6b8d0cea29e9d9779f192935ffa92eb
Hisham Amery <hishamamery@gmail.com>
2020-08-26 17:48:31 -0600

    Updates

---
+++ precache-manifest.2010826dbd87935c3f61ac4a0372bf2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac10392238a0c5d86b7cff7b48ea92d",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "d4ec43c6b17e6174e1ca",
+    "url": "/Employee-Directory/static/css/main.5de59180.chunk.css"
+  },
+  {
+    "revision": "6fa2b397350ed249f39f",
+    "url": "/Employee-Directory/static/js/2.683fd4ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.683fd4ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4ec43c6b17e6174e1ca",
+    "url": "/Employee-Directory/static/js/main.7b519169.chunk.js"
+  },
+  {
+    "revision": "52e1e8e23e7dfaf64514",
+    "url": "/Employee-Directory/static/js/runtime-main.6ca9f082.js"
+  }
+]);
