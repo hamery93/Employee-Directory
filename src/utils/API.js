1ab193bc9c1cbaaf7da81cab9cf2e06b220d97a0
Hisham Amery <hishamamery@gmail.com>
2020-08-29 20:40:16 -0600

    Fixed readme

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default {
   get: function() {
-    return axios.get("https://randomuser.me/api/?results=250&nat=us");
+    return axios.get("https://randomuser.me/api/?results=150&nat=us");
   }
 };
